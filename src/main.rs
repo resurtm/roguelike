510b260708aec289ecf72ecc63c5c58df020ce16
resurtm <resurtm@gmail.com>
2024-12-07 09:32:38 +0100

    Simple movements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -14,19 +15,13 @@ pub fn main() {
         .build()
         .unwrap();
     let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
+
+    let size = 20f64;
+    let velocity = 15f64;
+    let mut pos = (200f64, 300f64);
 
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
-
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -34,11 +29,38 @@ pub fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::UP),
+                    ..
+                } => pos.1 -= velocity,
+                Event::KeyDown {
+                    keycode: Some(Keycode::DOWN),
+                    ..
+                } => pos.1 += velocity,
+                Event::KeyDown {
+                    keycode: Some(Keycode::LEFT),
+                    ..
+                } => pos.0 -= velocity,
+                Event::KeyDown {
+                    keycode: Some(Keycode::RIGHT),
+                    ..
+                } => pos.0 += velocity,
                 _ => {}
             }
         }
 
-        // game loop is here
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        canvas.set_draw_color(Color::RGB(255, 0, 0));
+        canvas
+            .draw_rect(Rect::new(
+                (pos.0 - size) as i32,
+                (pos.1 - size) as i32,
+                (size * 2.0) as u32,
+                (size * 2.0) as u32,
+            ))
+            .unwrap();
 
         canvas.present();
         sleep(Duration::new(0, 1_000_000_000u32 / 60));
