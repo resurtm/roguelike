093a401ca58267645d4698fdc788788827443426
resurtm <resurtm@gmail.com>
2025-02-15 11:25:59 +0100

    Clean-up `player.rs`

--- src/consts.rs
+++ src/consts.rs
@@ -1,5 +1,3 @@
-pub(crate) const START_POSITION: (f64, f64) = (450.0, 300.0);
-
 pub(crate) const WINDOW_TITLE: &str = "roguelike 🔮🧝🏻🪄";
 pub(crate) const WINDOW_SIZE: (u32, u32) = (1920, 1280);
 
--- src/player.rs
+++ src/player.rs
@@ -1,48 +1,47 @@
-use crate::{consts::START_POSITION, input::Input, video};
+use crate::{input::Input, video};
 use cgmath::{Point2, Vector2};
 use thiserror::Error;
 use wgpu::util::DeviceExt;
 
-pub(crate) struct PlayerOld {
-    pub(crate) position: Point2<f64>,
+// --------------------------------------------------
+// --- PLAYER ---
+// --------------------------------------------------
 
-    pub(crate) velocity: Vector2<f64>,
-    velocity_delta: f64,
-    velocity_max: f64,
-    velocity_slowdown: f64,
+/// Represents the player character.
+pub struct Player {
+    position: Point2<f32>,
 
-    pub(crate) is_attack: bool,
+    velocity: Vector2<f32>,
+    velocity_delta: f32,
+    velocity_max: f32,
+    velocity_slowdown: f32,
+
+    attack: bool,
+
+    pub mesh: Mesh,
 }
 
-impl PlayerOld {
-    pub(crate) fn new() -> PlayerOld {
-        PlayerOld {
-            position: Point2::new(START_POSITION.0, START_POSITION.1),
+impl Player {
+    /// Creates a new player character instance.
+    pub fn new(video: &video::Video) -> Result<Self, PlayerError> {
+        let mesh = Mesh::new(video)?;
+
+        Ok(Self {
+            position: Point2::new(-5.0, -5.0),
 
             velocity: Vector2::new(0.0, 0.0),
             velocity_delta: 0.35,
             velocity_max: 6.5,
             velocity_slowdown: 0.92,
 
-            is_attack: false,
-        }
+            attack: false,
+
+            mesh,
+        })
     }
 
-    pub(crate) fn apply_input(&mut self, input: &Input) {
-        if input.key_up {
-            self.velocity.y -= self.velocity_delta
-        }
-        if input.key_down {
-            self.velocity.y += self.velocity_delta
-        }
-        if input.key_left {
-            self.velocity.x -= self.velocity_delta
-        }
-        if input.key_right {
-            self.velocity.x += self.velocity_delta
-        }
-
-        self.is_attack = input.key_space;
+    /// Advance the player character state, physics, etc.
+    pub fn advance(&mut self) {
         self.position += self.velocity;
         self.velocity *= self.velocity_slowdown;
 
@@ -58,8 +57,28 @@ impl PlayerOld {
         if self.velocity.y < -self.velocity_max {
             self.velocity.y = -self.velocity_max;
         }
+
+        self.mesh.advance(self.position);
     }
 
+    /// Apply input to the player character state, physics, etc.
+    pub fn apply_input(&mut self, input: &Input) {
+        if input.key_up {
+            self.velocity.y -= self.velocity_delta
+        }
+        if input.key_down {
+            self.velocity.y += self.velocity_delta
+        }
+        if input.key_left {
+            self.velocity.x -= self.velocity_delta
+        }
+        if input.key_right {
+            self.velocity.x += self.velocity_delta
+        }
+        self.attack = input.key_space;
+    }
+
+    // TODO: Uncomment and rework this later.
     // pub(crate) fn sync_level_collision(&mut self, col: &crate::level::Collision) {
     //     let p = Aabb::new(
     //         Point2::new(self.position.x - 96.0 / 4.0, self.position.y - 96.0 / 4.0),
@@ -76,25 +95,6 @@ impl PlayerOld {
     // }
 }
 
-// --------------------------------------------------
-// --- PLAYER ---
-// --------------------------------------------------
-
-pub struct Player {
-    pub mesh: Mesh,
-}
-
-impl Player {
-    pub fn new(video: &video::Video) -> Result<Self, PlayerError> {
-        let mesh = Mesh::new(video)?;
-        Ok(Self { mesh })
-    }
-
-    pub fn advance(&mut self) {
-        self.mesh.advance();
-    }
-}
-
 #[derive(Error, Debug)]
 pub enum PlayerError {
     #[error("mesh error: {0}")]
@@ -105,12 +105,15 @@ pub enum PlayerError {
 // --- MESH ---
 // --------------------------------------------------
 
+// Represents a player character mesh.
 pub struct Mesh {
-    texture_id: TextureID,
     frame: f32,
     frame_max: f32,
+    position: Point2<f32>,
 
-    pub textures: [video::TextureGroup; MESH_TEXTURE_COUNT],
+    texture_id: TextureID,
+    texture_buffer: u32,
+    pub textures: [video::TextureGroup; TEX_COUNT],
 
     pub vertex_buffer: Vec<wgpu::Buffer>,
     #[allow(dead_code)]
@@ -125,27 +128,24 @@ pub struct Mesh {
 }
 
 impl Mesh {
+    /// Creates a new player character mesh instance.
     pub fn new(video: &video::Video) -> Result<Self, MeshError> {
         // textures
         let texture_id = TextureID::Orc3Idle;
-        let (mut textures, mut frame_max) = (vec![], 0.0);
-        for (index, (sub_path, col_count, _)) in MESH_TEXTURE_ID_LOOKUP.iter().enumerate() {
-            textures.push(video::TextureGroup::new(
-                video,
-                &std::fs::read(format!("{}{}", MESH_TEXTURE_PATH_PREFIX, sub_path))
-                    .map_err(MeshError::ReadIO)?,
-                &format!("{:?}", TextureID::from_index(index)),
-            )?);
-            if TextureID::from_index(index) == texture_id {
-                frame_max = *col_count as f32;
-            }
+        let mut textures = Vec::with_capacity(TEX_ID_LOOKUP.len());
+        for (idx, (sub_path, _, _)) in TEX_ID_LOOKUP.iter().enumerate() {
+            let bytes = &std::fs::read(format!("{}{}", TEX_PATH_PREFIX, sub_path))
+                .map_err(MeshError::ReadIO)?;
+            let label = format!("{:?}", TextureID::from_index(idx));
+            textures.push(video::TextureGroup::new(video, bytes, &label)?);
         }
         let textures = textures.try_into().map_err(|_| MeshError::ReadConvert)?;
 
         // geometry -- vertices
-        let (mut vertex_buffer, mut vertex_count) = (vec![], vec![]);
-        for (idx, &k) in [8, 6, 4].iter().enumerate() {
-            let (vs, vsc) = Self::build_vertices(k);
+        let mut vertex_buffer = Vec::with_capacity(TEX_COLUMN_COUNTS.len());
+        let mut vertex_count = Vec::with_capacity(TEX_COLUMN_COUNTS.len());
+        for (idx, col_count) in TEX_COLUMN_COUNTS.iter().enumerate() {
+            let (vs, vsc) = Self::build_vertices(*col_count);
             vertex_buffer.push(video.device.create_buffer_init(
                 &wgpu::util::BufferInitDescriptor {
                     label: Some(&format!("player_mesh_vertex_buffer_{}", idx)),
@@ -157,9 +157,10 @@ impl Mesh {
         }
 
         // geometry -- indices
-        let (mut index_buffer, mut index_count) = (vec![], vec![]);
-        for (idx, &vsc) in vertex_count.iter().enumerate() {
-            let (is, isc) = Self::build_indices(vsc);
+        let mut index_buffer = Vec::with_capacity(TEX_COLUMN_COUNTS.len());
+        let mut index_count = Vec::with_capacity(TEX_COLUMN_COUNTS.len());
+        for (idx, vsc) in vertex_count.iter().enumerate() {
+            let (is, isc) = Self::build_indices(*vsc);
             index_buffer.push(video.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
                 label: Some(&format!("player_mesh_index_buffer_{}", idx)),
                 contents: bytemuck::cast_slice(&is),
@@ -181,86 +182,102 @@ impl Mesh {
             entries: &[wgpu::BindGroupEntry { binding: 0, resource: buffer.as_entire_binding() }],
         });
 
+        let t = TEX_ID_LOOKUP[texture_id.index()];
         Ok(Self {
-            texture_id,
             frame: 0.0,
-            frame_max,
+            frame_max: t.1 as f32,
+            position: Point2::new(0.0, 0.0),
+
+            texture_id,
+            texture_buffer: t.2,
             textures,
+
             vertex_buffer,
             vertex_count,
+
             index_buffer,
             index_count,
+
             buffer,
             bind_group,
         })
     }
 
     /// Build vertices vector to be used to create a new vertex buffer.
-    /// Internal helper for [`new`].
-    fn build_vertices(k: u32) -> (Vec<crate::video::Vertex>, u32) {
-        let s = MESH_TEXTURE_TILE_SIZE as f32;
-        let (w, h) = (k as f32 * s, MESH_TEXTURE_HEIGHT as f32 * s);
-        let (mut vertices, mut vertex_count) = (vec![], 0);
-        for x in 0..k {
-            for y in 0..MESH_TEXTURE_HEIGHT {
+    /// Internal helper for [`new`]. Short variable names are more
+    /// readable here in this specific function
+    fn build_vertices(col_count: u32) -> (Vec<crate::video::Vertex>, u32) {
+        let s = TEX_TILE_SIZE as f32;
+        let (w, h) = (col_count as f32 * s, TEX_ROW_COUNT as f32 * s);
+
+        let vertex_count = VERTS_PER_TILE * TEX_ROW_COUNT * col_count;
+        let mut vertices = Vec::with_capacity(vertex_count as usize);
+
+        for x in 0..col_count {
+            for y in 0..TEX_ROW_COUNT {
                 let (x, y) = (x as f32, y as f32);
                 vertices.push(crate::video::Vertex::new(
-                    (-MESH_XZ_COORD, MESH_Y_COORD, -MESH_XZ_COORD).into(),
+                    (-VERT_XZ_COORD, VERT_Y_COORD, -VERT_XZ_COORD).into(),
                     ((s * x) / w, (s * y) / h).into(),
                 ));
                 vertices.push(crate::video::Vertex::new(
-                    (-MESH_XZ_COORD, MESH_Y_COORD, MESH_XZ_COORD).into(),
+                    (-VERT_XZ_COORD, VERT_Y_COORD, VERT_XZ_COORD).into(),
                     ((s * x) / w, (s * (y + 1.0)) / h).into(),
                 ));
                 vertices.push(crate::video::Vertex::new(
-                    (MESH_XZ_COORD, MESH_Y_COORD, MESH_XZ_COORD).into(),
+                    (VERT_XZ_COORD, VERT_Y_COORD, VERT_XZ_COORD).into(),
                     ((s * (x + 1.0)) / w, (s * (y + 1.0)) / h).into(),
                 ));
                 vertices.push(crate::video::Vertex::new(
-                    (MESH_XZ_COORD, MESH_Y_COORD, -MESH_XZ_COORD).into(),
+                    (VERT_XZ_COORD, VERT_Y_COORD, -VERT_XZ_COORD).into(),
                     ((s * (x + 1.0)) / w, (s * y) / h).into(),
                 ));
-                vertex_count += MESH_VERTICES_PER_TILE;
             }
         }
+
         (vertices, vertex_count)
     }
 
     /// Build indices vector to be used to create a new index buffer.
     /// Internal helper for [`new`].
     fn build_indices(vertex_count: u32) -> (Vec<u16>, u32) {
-        let (mut indices, mut index_count) = (vec![], 0);
-        for i in 0..(vertex_count / MESH_VERTICES_PER_TILE) as u16 {
-            let t = [0, 2, 3, 0, 1, 2].iter().map(|x| *x + i * MESH_VERTICES_PER_TILE as u16);
+        let index_count = vertex_count * INDS_PER_TILE;
+        let mut indices = Vec::with_capacity(index_count as usize);
+
+        for i in 0..(vertex_count / VERTS_PER_TILE) as u16 {
+            let t = TRI_INDS.iter().map(|x| *x + i * VERTS_PER_TILE as u16);
             indices.extend(t);
-            index_count += MESH_INDICES_PER_TILE;
         }
+
         (indices, index_count)
     }
 
-    fn advance(&mut self) {
-        self.frame += MESH_ANIM_SPEED;
-        let frame_max = MESH_TEXTURE_ID_LOOKUP[self.texture_id.index()].1 as f32;
-        if self.frame >= frame_max {
+    /// Advances internal mesh state changes. Such as animation, etc.
+    fn advance(&mut self, position: Point2<f32>) {
+        self.frame += ANIM_SPEED;
+        if self.frame >= self.frame_max {
             self.frame = 0.0;
         }
+        self.position = position;
     }
 
-    pub fn render(&self, video: &video::Video, render_pass: &mut wgpu::RenderPass) {
-        render_pass.set_bind_group(0, &self.textures[self.texture_id.index()].bind_group, &[]);
-        render_pass.set_bind_group(2, &self.bind_group, &[]);
+    /// Render mesh with its current given state based on provided video instance and render pass.
+    pub fn render(&self, vid: &video::Video, rp: &mut wgpu::RenderPass) {
+        rp.set_bind_group(0, &self.textures[self.texture_id.index()].bind_group, &[]);
+        rp.set_bind_group(2, &self.bind_group, &[]);
 
-        let buf = MESH_TEXTURE_ID_LOOKUP[self.texture_id.index()].2 as usize;
-        render_pass.set_vertex_buffer(0, self.vertex_buffer[buf].slice(..));
-        render_pass.set_index_buffer(self.index_buffer[buf].slice(..), wgpu::IndexFormat::Uint16);
+        let t = self.texture_buffer as usize;
+        rp.set_vertex_buffer(0, self.vertex_buffer[t].slice(..));
+        rp.set_index_buffer(self.index_buffer[t].slice(..), wgpu::IndexFormat::Uint16);
 
         let m = video::MatrixUniform {
-            mat: cgmath::Matrix4::from_translation((-5.0f32, 0.0f32, -5.0f32).into()).into(),
+            mat: cgmath::Matrix4::from_translation((self.position.x, 0.0, self.position.y).into())
+                .into(),
         };
-        video.queue.write_buffer(&self.buffer, 0, bytemuck::cast_slice(&m.mat));
+        vid.queue.write_buffer(&self.buffer, 0, bytemuck::cast_slice(&m.mat));
 
         let base_index = (self.frame as u32 * 4) * 6;
-        render_pass.draw_indexed(base_index..base_index + 6, 0, 0..1);
+        rp.draw_indexed(base_index..base_index + 6, 0, 0..1);
     }
 }
 
@@ -276,7 +293,7 @@ pub enum MeshError {
     Texture(#[from] crate::video::TextureError),
 }
 
-// TODO: add Orc1 and Orc2, and this will fix the Clippy disablement below
+// TODO: add Orc1 and Orc2 textures, after that there will be no need in `#[allow(...)]` below.
 #[allow(clippy::enum_variant_names)]
 #[derive(Debug, PartialEq)]
 enum TextureID {
@@ -291,6 +308,7 @@ enum TextureID {
 }
 
 impl TextureID {
+    /// Given an enum instance, provide index based on it.
     fn index(&self) -> usize {
         match *self {
             Self::Orc3Attack => 0,
@@ -304,6 +322,7 @@ impl TextureID {
         }
     }
 
+    /// Given index, provide an enum instance based on it.
     fn from_index(index: usize) -> Self {
         match index {
             0 => Self::Orc3Attack,
@@ -319,7 +338,9 @@ impl TextureID {
     }
 }
 
-const MESH_TEXTURE_ID_LOOKUP: [(&str, u32, u32); MESH_TEXTURE_COUNT] = [
+/// Tuple is a) the texture image file path, b) columns count in the image,
+/// and c) vertex/index buffer index to be used to draw it.
+const TEX_ID_LOOKUP: [(&str, u32, u32); TEX_COUNT] = [
     ("attack/orc3_attack_full.png", 8, 0), // TextureID::Orc3Attack
     ("death/orc3_death_full.png", 8, 0),   // TextureID::Orc3Death
     ("hurt/orc3_hurt_full.png", 6, 1),     // TextureID::Orc3Hurt
@@ -329,13 +350,20 @@ const MESH_TEXTURE_ID_LOOKUP: [(&str, u32, u32); MESH_TEXTURE_COUNT] = [
     ("walk/orc3_walk_full.png", 6, 1),     // TextureID::Orc3Walk
     ("walk_attack/orc3_walk_attack_full.png", 6, 1), // TextureID::Orc3WalkAttack
 ];
-const MESH_TEXTURE_PATH_PREFIX: &str = "./assets/orc/png/Orc3/orc3_";
-const MESH_TEXTURE_COUNT: usize = 8;
 
-const MESH_TEXTURE_HEIGHT: u32 = 4;
-const MESH_TEXTURE_TILE_SIZE: u32 = 64;
-const MESH_VERTICES_PER_TILE: u32 = 4;
-const MESH_INDICES_PER_TILE: u32 = 6;
-const MESH_XZ_COORD: f32 = 1.0;
-const MESH_Y_COORD: f32 = -0.25;
-const MESH_ANIM_SPEED: f32 = 0.15;
+const TEX_COUNT: usize = 8;
+const TEX_PATH_PREFIX: &str = "./assets/orc/png/Orc3/orc3_";
+const TEX_COLUMN_COUNTS: [u32; 3] = [8, 6, 4];
+const TEX_ROW_COUNT: u32 = 4;
+const TEX_TILE_SIZE: u32 = 64;
+
+const VERTS_PER_TILE: u32 = 4;
+const INDS_PER_TILE: u32 = 6;
+
+/// Two triangles, every of 3 indices/vertices.
+const TRI_INDS: [u16; 6] = [0, 2, 3, 0, 1, 2];
+
+const VERT_XZ_COORD: f32 = 1.0;
+const VERT_Y_COORD: f32 = -0.25;
+
+const ANIM_SPEED: f32 = 0.15;
